ae2611506c1dce45daae5472483ed2f69e90aad9
holaliamewu <me.amewu@gmail.com>
2024-06-04 23:46:15 +0000

    fix: speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/App'
-import { injectSpeedInsights  } from "@vercel/speed-insights/next"
+import { SpeedInsights  } from "@vercel/speed-insights/next"
 
-injectSpeedInsights();
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <SpeedInsights />
     <App />
   </React.StrictMode>,
 )
