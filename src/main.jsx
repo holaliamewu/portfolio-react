7abc08f92c16a8e242f40363247bb98a4010a27c
holaliamewu <me.amewu@gmail.com>
2024-06-04 23:41:24 +0000

    feat: vercel speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/App'
+import { injectSpeedInsights  } from "@vercel/speed-insights/next"
+
+injectSpeedInsights();
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
