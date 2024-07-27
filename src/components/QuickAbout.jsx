117b735b146abf0c7288d3ddd9100af02728ee14
holaliamewu <me.amewu@gmail.com>
2024-07-27 15:54:52 +0000

    fix: no word spacing on mobile

--- src/components/QuickAbout.jsx
+++ src/components/QuickAbout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function QuickAbout() {
     return(
         <div className=" xs:w-[90%] md:w-[80%] mr-auto xs:font-[400] 
-        md:font-[400] xs:text-[17px] text-[rgba(0,0,0,0.7)] md:text-[15px] tracking-wide ">
+        md:font-[400] xs:text-[17px] text-[rgba(0,0,0,0.7)] md:text-[15px] md:tracking-wide ">
             <p className="leading-[1.8]b text-justify "
             >👋. front-end software engineer. i curate aesthetically-pleasing
              stuff on the web, paying attention to accessibillity. i'm intrigued
