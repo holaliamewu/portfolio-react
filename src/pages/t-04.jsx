5db275e0f8791dc3281169752a805eaa01d03f66
Amewu Emmanuel Mensah <me.amewu@gmail.com>
2025-09-23 17:54:15 +0000

    quick edits 1.1

--- src/pages/t-04.jsx
+++ src/pages/t-04.jsx
@@ -12,7 +12,7 @@ return (
             className='flex gap-1 items-center font-[700] text-zinc-800 text-lg text-left mt-[100px]' 
             >jean-michel basquiat</h1>
             <div className='flex flex-col gap-6 text-sm leading-[1.8] md:leading-[2] mt-4 md:mt-6  font-light 
-            leading-[1.5] text-justify hyphens-auto' 
+            leading-[1.5] text-justify hyphens-auto lowercase' 
             >
                 <p>
                     I just finished watching some documentaries on Jean-Michel Basquiat and just like everyone else who encounters him, I don&apos;t know how or what to feel.
@@ -27,19 +27,22 @@ return (
                     What made Basquiat who he was was his ability to ‘break the rules’  and let his ‘feelings’ or emotions take over. But this was a problem.
                 </p>
                 <p>
-                    ‘Breaking the rules’ wasn&apos;t really a problem for Basquiat since it is actually what made him the distinct graffiti artist he was. His problem was his ‘feelings’ - let&apos;s call it his emotions.
+                    ‘Breaking the rules’ wasn&apos;t really a problem for Basquiat since it&apos;s actually what made him the distinct graffiti artist he was. His problem was his ‘feelings’ - let&apos;s call it his emotions.
                 </p>
                 <p>
-                    Basquiat was able to make all these great artworks due to the fact that he could tap into his ‘feelings’ about a book he had read or something happening around him. Basquiat once said he was ‘fuelled’ by anger most of the times he made art.
+                    Basquiat was able to make all these great artworks due to the fact that he could tap into his ‘feelings’ about his experiences; a book he had read or something happening around him. Basquiat once said he was ‘fuelled’ by anger most of the times he made art.
                 </p>
                 <p>
                     Whiles this served him well to a point, It is actually what killed Basquiat (sadly).
                 </p>
                 <p>
-                    Basquiat untimely death was recorded to be due to heroine overdose after his icon and mentor Andy Warhol died at 58.
+                    Basquiat&apos;s untimely death was recorded to be due to heroine overdose after his icon and mentor Andy Warhol died at 58.
                 </p>
                 <p>
-                    An artist&apos;s greatest ability is not just what he produces, but knowing when to use their ‘feelings’ and emotions to make art. ‘Feelings’ and emotions are tools, just like guns, They can hunt and they can hurt (or even this case, kill).
+                    Sometimes, moderation is as powerful as going mountains deep into your 'feelings'.
+                </p>
+                <p>
+                    Your greatest ability is not just what he produces, but knowing when to use their ‘feelings’ and emotions to make art. ‘Feelings’ and emotions are tools, just like guns, They can hunt and they can hurt (or even this case, kill).
                 </p>
                 <p>
                     Rest on, Basquiat. 
